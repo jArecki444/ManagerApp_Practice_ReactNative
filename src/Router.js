1c388330476f74fa75d0f42c6e3425db28bb5c3e
Arkadiusz Cichurski <arkadiusz.cichurski@itcraft.pl>
2019-06-18 15:37:01 +0200

    Create route for employe create form

--- src/Router.js
+++ src/Router.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Scene, Router } from 'react-native-router-flux';
+import { Scene, Router, Actions } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
 import EmployeeList from './components/EmployeeList';
+import EmployeeCreate from './components/EmployeeCreate';
 
 const RouterComponent = () => {
   return (
@@ -12,14 +13,21 @@ const RouterComponent = () => {
             key="login"
             component={LoginForm}
             title="Please Login"
-            initial
           />
         </Scene>
         <Scene key="main">
           <Scene
+            onRight={() => Actions.employeeCreate(null)}
+            rightTitle="Add"
             key="employeeList"
             component={EmployeeList}
             title="Employees"
+            initial
+          />
+          <Scene
+            key="employeeCreate"
+            component={EmployeeCreate}
+            title="Create employee"
           />
         </Scene>
       </Scene>
