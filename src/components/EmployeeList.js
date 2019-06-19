6e4611abf7958812de5e104df23b75c683a4e13d
Arkadiusz Cichurski <arkadiusz.cichurski@itcraft.pl>
2019-06-19 12:24:40 +0200

    Displaying list of employees using ListView

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,28 +1,58 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { ListView } from 'react-native';
 import { connect } from 'react-redux';
 import { employeesFetch } from '../actions';
+import _ from 'lodash';
+import EmployeeListItem from './EmployeeListItem';
 
 class EmployeeList extends Component {
-    componentWillMount() {
-        this.props.employeesFetch();
-    }
+  componentWillMount() {
+    this.props.employeesFetch();
+    this.createDataSource(this.props);
+  }
 
-    render() {
-        return (
-            <View>
-                <Text>Employee</Text>
-                <Text>Employee</Text>
-                <Text>Employee</Text>
-                <Text>Employee</Text>
-                <Text>Employee</Text>
-                <Text>Employee</Text>
-                <Text>Employee</Text>
-                <Text>Employee</Text>
-            </View>
-        );
-    }
+  componentWillReceiveProps(nextProps) {
+    // nextProps are the next set of props that this component
+    // will be rendered with
+    // this.props is still the old set of props
+
+    this.createDataSource(nextProps);
+  }
+
+  createDataSource({ employees }) {
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2
+    });
+
+    this.dataSource = ds.cloneWithRows(employees);
+  }
+
+  renderRow(employee) {
+    return <EmployeeListItem employee={employee} />;
+  }
+
+  render() {
+    console.log('props', this.props);
+    return (
+      <ListView
+        enableEmptySections
+        dataSource={this.dataSource}
+        renderRow={this.renderRow}
+      />
+    );
+  }
 }
 
+const mapStateToProps = state => {
+  // Transforming object from firebase (state.employees) to array
+  const employees = _.map(state.employees, (val, uid) => {
+    return { ...val, uid };
+  });
 
-export default connect(null, { employeesFetch }) (EmployeeList);
+  return { employees };
+};
+
+export default connect(
+  mapStateToProps,
+  { employeesFetch }
+)(EmployeeList);
---
+++ src/components/EmployeeListItem.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { Text, StyleSheet } from 'react-native';
+import { CardItem } from './common';
+
+class ListItem extends Component {
+    render() {
+        const { name } = this.props.employee;
+
+        return (
+            <CardItem>
+                <Text style={StyleSheet.titleStyle}>
+                {name}
+                </Text>
+            </CardItem>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    titleStyle: {
+        fontSize: 18,
+        paddingLeft: 15
+    }
+});
+
+export default ListItem;
